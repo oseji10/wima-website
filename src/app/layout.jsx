81b4e33f161f8abb6d7bc237b528802d1108750d
Victor Oseji <vctroseji@gmail.com>
2025-07-10 23:09:56 +0100

    feat: add changes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,8 +9,49 @@ import RouteScrollToTop from "@/helper/RouteScrollToTop";
 import ColorPalate from "@/helper/ColorPalate";
 
 export const metadata = {
-  title: "WIMA | Women in Mechanized Agriculture",
-  description: "Empowering women one community at a time",
+ title: {
+    default: 'Women In Mechanized Agriculture - WIMA', // Default title for all pages
+    template: '%s | Women In Mechanized Agriculture - WIMA', // Template for page-specific titles
+  },
+  description: "WIMA is the first and only women-led agricultural mechanization association in Nigeria, dedicated to empowering women farmers through access to modern farming",
+  keywords: ['WIMA', 'women in agriculture', 'women in nigeria', 'mechanized agriculture', 'mechanized agriculture in nigeria', 
+    'nigerian women', 'farming women', 'tractor', 'women in tractor', 'tractor nigerai', 'hello tractor'
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: 'WIMA | Women in Mechanized Agriculture',
+    description: 'WIMA is the first and only women-led agricultural mechanization association in Nigeria, dedicated to empowering women farmers through access to modern farming',
+    url: 'https://wimanigeria.com',
+    siteName: 'Women In Mechanized Agriculture',
+    images: [
+      {
+        url: 'https://new.wimanigeria.com/assets/images/wima-base.png',
+        width: '100%',
+        height:  '100%',
+        alt: 'WIMA Logo',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'WIMA | Women In Mechanized Agriculture',
+    description: 'WIMA is the first and only women-led agricultural mechanization association in Nigeria, dedicated to empowering women farmers through access to modern farming',
+    images: ['https://new.wimanigeria.com/assets/images/wima-base.png'],
+  },
+
+  
 };
 
 export default function RootLayout({ children }) {
